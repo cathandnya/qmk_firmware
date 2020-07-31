2ea339de205560fa22d71af6814fe9b6c6d2f5fd
nya <home@cathand.org>
2020-07-31 11:17:33 +0900

    Update keymap.c

--- keyboards/dz60/keymaps/custom/keymap.c
+++ keyboards/dz60/keymaps/custom/keymap.c
@@ -7,19 +7,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * | Tab    | Q   | W   | E   | R   | T   | Y   | U   | I   | O   | P   | [   | ]   |   \    |
    * |-----------------------------------------------------------------------------------------+
-   * | Caps     | A   | S   | D   | F   | G   | H   | J   | K   | L   | ;   | '   | Enter      |
+   * | Ctrl     | A   | S   | D   | F   | G   | H   | J   | K   | L   | ;   | '   | Enter      |
    * |-----------------------------------------------------------------------------------------+
    * | Shift     | Z   | X   | C   | V   | B   | N   | M   | ,   | .   | /   | ` ~ | Up |  Fn  |
    * |-----------------------------------------------------------------------------------------+
-   * | Ctrl | Alt  | Cmd  |               Space                |  Cmd  | Alt | Left| Down|Right|
+   * |  Fn  | Alt  | Cmd  |               Space                |  Cmd  | Alt | Left| Down|Right|
    * `-----------------------------------------------------------------------------------------'
    */
   LAYOUT_all(
     KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_GRV, KC_UP, MO(1),
-    KC_LCTL, KC_LALT, KC_LGUI, KC_NO, KC_SPC, KC_NO, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT),
+    MO(1), KC_LALT, KC_LGUI, KC_NO, KC_SPC, KC_NO, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT),
 
   /* ,-----------------------------------------------------------------------------------------.
    * |     |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |           |
